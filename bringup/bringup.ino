533707fe3af08f19927714107c82ebe87d7d0d8a
Brian Gomberg <briangomberg@gmail.com>
2015-08-23 14:27:34 -0700

    Fixed lcdPrintf line.

--- bringup/bringup.ino
+++ bringup/bringup.ino
@@ -8,6 +8,6 @@ void setup() {
 void loop() {
   roboShield.setLED(roboShield.buttonPressed());
   roboShield.lcdClear();
-  roboShield.lcdPrintf("HELLO\nTIME: %d", millis());
+  roboShield.lcdPrintf("HELLO\nTIME: %lu", millis());
   delay(50);
 }
